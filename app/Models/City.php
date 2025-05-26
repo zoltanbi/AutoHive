29cb765782264ed1e83be03649215fa0ddefd2e4
Zoltanbi <zoltanbiro001@gmail.com>
2025-05-26 16:41:48 -0700

    feat(models): add relationships to related models

--- app/Models/City.php
+++ app/Models/City.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class City extends Model
 {
@@ -12,4 +14,14 @@ class City extends Model
     public $timestamps = false;
 
     protected $fillable = ['name', 'province_id'];
+
+    public function province() : BelongsTo
+    {
+        return $this->belongsTo(Province::class);
+    }
+
+    public function cars(): HasMany
+    {
+        return $this->hasMany(Car::class);
+    }
 }
--- app/Models/Model.php
+++ app/Models/Model.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model as EloquentModel;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Model extends EloquentModel
 {
@@ -12,4 +14,14 @@ class Model extends EloquentModel
     public $timestamps = false;
 
     protected $fillable = ['name', 'maker_id'];
+
+    public function maker() : BelongsTo
+    {
+        return $this->belongsTo(Maker::class);
+    }
+
+    public function cars(): HasMany
+    {
+        return $this->hasMany(Car::class);
+    }
 }
